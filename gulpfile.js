a1c5be78c92cba304296a7075c4c1cbf29b127d0
Jordan Etzler <jozza24@gmail.com>
2017-09-27 13:47:38 +0930

    Overall Color Change of Website

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp');
 var path = require('path');
 var runSeq = require('run-sequence');
 
-// const PORT = 3000;
-const PORT = process.env.PORT
+const PORT = 3000;
+// const PORT = process.env.PORT
 const PLUGINS = {};
 
 PLUGINS.util = require('gulp-util');
